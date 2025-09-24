40a63e120fc9027214677bc8f620269f8edbb9ef
Alvaro Cristian Melean Humerez <160805972+A1v4r0@users.noreply.github.com>
2025-09-23 22:42:40 -0400

    Delete Practica 1 directory

--- Practica 1/ejercicio10.js
+++
@@ -1,8 +0,0 @@
-/*  10. ¿Cuando es conveniente utilizar un callback, y cuando es necesario utilizar una
-    promesa?
- 
-    R. Un Callback se usa para tareas simples o realizar tareas asincronicas sencillas.
-
-    Una Promesa es necesaria cuando hay varias operaciones asincronicas
-    encadenadas, y para evitar varias callbacks anidados.
-*/
--- Practica 1/ejercicio6.js
+++
@@ -1,9 +0,0 @@
-//6. Tomar los dos primeros elementos de un arreglo y almacenarlos en 
-// dos variables mediante desestructuración.
-
-let array=[1,"hola",37,5];
-
-let [a,b]=array;
-
-console.log(a);
-console.log(b);
